0f0877394daad936c0f95bdc695f2d3393e13000
Carlos <carlos_elm18@hotmail.com>
2023-04-19 11:49:48 -0500

    Avance Pregunta 1.3

--- Codigo R.R
+++ Codigo R.R
@@ -1,3 +1,30 @@
+#Pregunta 1.1
+library(httr)
+library(XML)
 html <-  GET("https://www.mediawiki.org/wiki/MediaWiki")
 content <-  content(html, as= "text")
 parsedHtml <-  htmlParse (content, asText = TRUE)
+
+
+#Pregunta 1.2
+
+title <- xpathSApply(parsedHtml,  "//title", xmlValue)
+texts <- xpathSApply(parsedHtml, "//p", xmlValue)
+
+images_url <- xpathSApply(parsedHtml, "//img", xmlGetAttr, 'src')
+
+#Pregunta 1.3
+
+links_text <- xpathSApply(parsedHtml, "//a", xmlValue)
+links_url <- xpathSApply(parsedHtml, "//a", xmlGetAttr, 'href')
+
+#Comprobando valores nulos
+
+valores_nulos <-  sapply(links_text, is.null)
+
+#Asignar valor por defecto NA a valores nulos
+
+valores_nulos <-  NA 
+
+valores
+
